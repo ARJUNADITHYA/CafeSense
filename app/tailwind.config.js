3f9da92cc0cf75df0a9bff95c4bb278a81448e16
ARJUNADITHYA <arju22003.cd@rmkec.ac.in>
2025-12-27 15:04:38 +0530

    Initial commit

---
+++ app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./global.css "],
+  theme: {
+    extend: {
+      colors: {
+        app_orange_color: '#C67C4E',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
+
